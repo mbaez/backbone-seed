eab55853ce7e1650056fc94ad74f6629b2bb1870
Maximiliano Báez <mxbg.py@gmail.com>
2013-10-21 19:07:28 -0300

    Se actualiza el setup

--- app/scripts/setup.js
+++ app/scripts/setup.js
@@ -1,16 +1,29 @@
+/**
+ * @namespace Contiene los metodos utilizados en la aplicación.
+ */
+var MyApp = {};
+
+
+MyApp.baseURL = "";
+MyApp.RESTBaseUrl = '/backbone-seed/';
+MyApp.RESTVersion = "";
+
 
 /**
  * Se configuran las dependencias de los módulo con requierejs
  */
 requirejs.config({
-    baseUrl: '/mimundo/',
+    baseUrl: '/backbone-seed/',
     paths: {
-        'text' : 'libs/backbone/text',
+        'text' : 'libs/require/text',
+        //~ jquery y sus plugins
         'jquery' : 'libs/jquery/jquery',
         'pnotify' : 'libs/jquery/jquery.pnotify.min',
+        //~ backbone y sus plugins
         'backbone' : 'libs/backbone/backbone',
         'underscore' : 'libs/backbone/underscore',
-        'backbone.extend' : 'scripts/common/backbone.extend'
+        //~ bootstrap y sus plugins
+        'bootstrap' : 'libs/bootstrap/bootstrap.min'
     },
     waitSeconds: 15,
     shim: {
@@ -32,9 +45,31 @@ requirejs.config({
             exports: 'Backbone'
         },
 
-        'backbone.extend': {
-            deps: ['jquery','backbone','pnotify'],
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'Bootstrap'
+        },
+
+        'scripts/common/backbone.extend': {
+            deps: ['jquery','backbone','pnotify',
+                'scripts/common/backbone.cache'],
             exports: 'BackboneExtend'
+        },
+
+        'scripts/common/backbone.page': {
+            deps: ['scripts/common/backbone.extend'],
+            exports: 'BackbonePage'
+        },
+
+        'scripts/common/backbone.cache': {
+            deps: ['backbone'],
+            exports: 'BackboneCache'
+        },
+
+        'scripts/common/app-router': {
+            deps: ['scripts/common/backbone.extend',
+                'scripts/common/backbone.page' ],
+            exports: 'AppRouter'
         }
     }
 });
@@ -49,13 +84,10 @@ requirejs.onError = function (err) {
     console.error(err);
 };
 
-require(["underscore",
-         'backbone',
-         'backbone.extend'
-    ],
-    function(_, Backbone, BackboneExtend){
+require(['scripts/common/app-router', 'bootstrap'], function(){
         Setup.sync();
         //se instancia el router de la aplicación.
+        var router = new Backbone.AppRouter();
         // se inicializa el historial
         Backbone.history.start();
     }
